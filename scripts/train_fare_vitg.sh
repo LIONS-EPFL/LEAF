da57988a0dddbc026f85a94d724a4dbe6495b9f6
Christian Schlarmann <christian.schlarmann@uni-tuebingen.de>
2025-06-02 09:41:23 +0200

    add fare training scripts

---
+++ scripts/train_fare_vitg.sh
@@ -0,0 +1,11 @@
+export PYTHONPATH="$PYTHONPATH:./src"
+
+python src/robust_vlm/train/adversarial_training_clip.py \
+--model_name hf-hub:laion/CLIP-ViT-g-14-laion2B-s12B-b42K --dataset imagenet \
+--imagenet_root /path/to/imagenet/ \
+--template ensemble --output_normalize False \
+--steps 10000 --warmup 700 --batch_size 64 \
+--loss l2 --opt adamw --lr 1e-5 --wd 1e-4 --attack pgd \
+--inner_loss l2 --norm linf --eps 2 \
+--iterations_adv 10 --stepsize_adv 1 \
+--wandb True --experiment_name FARE2 --log_freq 10
